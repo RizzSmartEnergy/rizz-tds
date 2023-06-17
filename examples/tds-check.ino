42931695baa596554b6f9cef72ce04ee96f7f57a
Haidar Ali <haidarali.eu@gmail.com>
2023-06-17 15:12:21 +0700

    change library name

--- examples/tds-check.ino
+++ examples/tds-check.ino
@@ -1,11 +1,11 @@
 #include <Arduino.h>
-#include <tds.h>
+#include <TDS.h>
 
 #define SENSOR_PIN 32
 #define VREF 3.3    //voltage reference
 #define AREF 4095   //analog range
 
-tds sensor(SENSOR_PIN, VREF, AREF);
+TDS sensor(SENSOR_PIN, VREF, AREF);
 
 void setup(){
   sensor.begin(115200); //baudrate
