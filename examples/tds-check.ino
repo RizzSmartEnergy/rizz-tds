388c22b05e02d2fc21bd5529bb2d7aeefb0001f7
Haidar Ali <haidarali.eu@gmail.com>
2023-06-23 23:09:26 +0700

    change voltage reference

--- examples/tds-check.ino
+++ examples/tds-check.ino
@@ -2,7 +2,7 @@
 #include <TDS.h>
 
 #define SENSOR_PIN 32
-#define VREF 3.3    //voltage reference
+#define VREF 3300    //voltage reference
 #define AREF 4095   //analog range
 
 TDS sensor(SENSOR_PIN, VREF, AREF);
