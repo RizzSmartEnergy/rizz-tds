17b38f7d26b45d9cf2b877485ab3f9a44ffb5702
Haidar Ali <haidarali.eu@gmail.com>
2023-06-23 15:43:30 +0700

    lint

--- src/tds.h
+++ src/tds.h
@@ -3,26 +3,28 @@
 
 #include <Arduino.h>
 
-class TDS{
-    public:
+class TDS
+{
+public:
     TDS(uint8_t pin, double vref, double aref);
     ~TDS();
     void begin(int baudrate);
     void setTemperature(float temp);
-    void getTemperature();
-    float getVoltageTDS();
+    float getTemperature();
     float getAnalogTDS();
-    int getMedianNum(int bArray[], int iFilterLen);
+    float getVoltageTDS();
+    int getMedianTDS(int bArray[], int iFilterLen);
+    float samplingTDS();
     float getEC();
     float getTDS();
     float getResistivity();
-    void print(int delay_time);
+    void getAllTDSData(int delay_time);
 
-    private:
+private:
     uint8_t _pin;
-    int _baudrate, _delay_time;
     double _vref, _aref;
     float _temp;
+    int _baudrate, _delay_time;
 };
 
 #endif
