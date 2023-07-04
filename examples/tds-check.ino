a7a38f5c583c538cf904851b7ba10e2dcff75899
Haidar Ali <haidarali.eu@gmail.com>
2023-07-05 00:13:05 +0700

    change parameter

--- examples/tds-check.ino
+++ examples/tds-check.ino
@@ -1,19 +1,20 @@
-// #include <Arduino.h>
-// #include <TDS.h>
+#include <Arduino.h>
+#include <TDS.h>
 
-// #define SENSOR_PIN 32
-// #define VREF 3300    //voltage reference
-// #define AREF 4095   //analog range
+#define SENSOR_PIN 32
+#define VREF 3300
+#define AREF 4095
 
-// TDS sensor(SENSOR_PIN, VREF, AREF);
+TDS sensor(SENSOR_PIN, VREF, AREF);
 
-// void setup(){
-//   Serial.begin(115200);
-//   sensor.begin();
-// }
+void setup(){
+  Serial.begin(115200);
+  sensor.begin();
+}
 
-// void loop(){
-//   sensor.setTemperature(25.0);
-//   sensor.getAllTDSData();
-//   delay(1000);
-// }
+void loop(){
+  sensor.setTemperature(25.0);
+  sensor.run();
+  sensor.getAllTDSData();
+  delay(1000);
+}
